9f604364f86c0cefd325f63ba72936a069e86b12
lucasgonzalezgagliardo <lucas.e.gonzalez@gmail.com>
2023-03-09 18:09:15 +0100

    home a la mitad

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,53 +1,70 @@
 const express = require("express");
 const router = express.Router();
-const Post = require("../models/Post.model")
+const Post = require("../models/Post.model");
 
 router.get("/", (req, res, next) => {
-
-  res.json("All good in here2");
-  })
-
-
-
-
+  Post.find()
+    .then((result) => {
+      console.log("RESULT", result);
+      res.json(result);
+    })
+    .catch((err) => next(err));
+});
 
 router.post("/new", (req, res, next) => {
-    res.json("post a post xD")
+  const { contract, image, description, batch, price, category, available } =
+    req.body;
+  Post.create({
+    contract,
+    image,
+    description,
+    batch,
+    price,
+    category,
+    available,
+  })
+    .then((result) => {
+      console.log("RESULT", result);
+      res.json(result);
+    })
+    .catch((err) => next(err));
 });
 
 router.get("/:postId", (req, res, next) => {
-    //const {contract, image, description, batch, price, category, available} = req.body;
-    const {postId} = req.params;
-    console.log({postId});
-    Post.findById(postId)
-    .then((result)=>{
-        console.log("RESULT", result)
-    res.json("get a post Id");
+  const { postId } = req.params;
+  console.log({ postId });
+  Post.findById(postId)
+    .then((result) => {
+      console.log("RESULT", result);
+      res.json(result);
     })
-    .catch(err => console.log(err))
-    });
-
-router.put("/:postId/edit", (req, res, next) =>{
-    const {contract, image, description, batch, price, category, available} = req.body;
-    const{postId} = req.params;
-    console.log("POST", req.body)
-    Post.findByIdAndUpdate(postId, {contract, image, description, batch, price, category, available}, {new: true})
-    .then(result => {
-        console.log("RESULT", result)
-        res.json("post a edit4 ")
-    })
-    .catch(err => console.log(err))
+    .catch((err) => next(err));
 });
 
-
-    router.delete("/:postId/delete", (req, res, next) => {
-    const {postId} = req.params;
-    Post.findByIdAndDelete(postId)
-    .then(result => {
-        res.json("delete a post4 ")
+router.put("/:postId/edit", (req, res, next) => {
+  const { contract, image, description, batch, price, category, available } =
+    req.body;
+  const { postId } = req.params;
+  console.log("POST", req.body);
+  Post.findByIdAndUpdate(
+    postId,
+    { contract, image, description, batch, price, category, available },
+    { new: true }
+  )
+    .then((result) => {
+      console.log("RESULT", result);
+      res.json(result);
     })
-    .catch(err => console.log(err))
-})
+    .catch((err) => next(err));
+});
 
+router.delete("/:postId/delete", (req, res, next) => {
+  const { postId } = req.params;
+  Post.findByIdAndDelete(postId)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = router;
