03269be2b8be30bf4a626770c88cef97e62b094a
lucasgonzalezgagliardo <lucas.e.gonzalez@gmail.com>
2023-03-09 09:26:10 +0100

    delete working

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,16 +3,29 @@ const router = express.Router();
 const Post = require("../models/Post.model")
 
 router.get("/", (req, res, next) => {
+
   res.json("All good in here2");
-});
+  })
+
+
+
+
 
 router.post("/new", (req, res, next) => {
     res.json("post a post xD")
 });
 
 router.get("/:postId", (req, res, next) => {
-    res.json("get a post ID")
-});
+    //const {contract, image, description, batch, price, category, available} = req.body;
+    const {postId} = req.params;
+    console.log({postId});
+    Post.findById(postId)
+    .then((result)=>{
+        console.log("RESULT", result)
+    res.json("get a post Id");
+    })
+    .catch(err => console.log(err))
+    });
 
 router.put("/:postId/edit", (req, res, next) =>{
     const {contract, image, description, batch, price, category, available} = req.body;
@@ -25,6 +38,8 @@ router.put("/:postId/edit", (req, res, next) =>{
     })
     .catch(err => console.log(err))
 });
+
+
     router.delete("/:postId/delete", (req, res, next) => {
     const {postId} = req.params;
     Post.findByIdAndDelete(postId)
